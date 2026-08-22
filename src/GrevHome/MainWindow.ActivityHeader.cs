c671bce7d2977ecbe16e7e9160213973f170876c
Grevyo <127150029+Grevyo@users.noreply.github.com>
2026-08-22 21:15:41 +0100

    Fix volume quick-control WPF namespace

--- src/GrevHome/MainWindow.ActivityHeader.cs
+++ src/GrevHome/MainWindow.ActivityHeader.cs
@@ -1,5 +1,6 @@
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Media;
 using System.Windows.Threading;
 using GrevHome.Machine;
