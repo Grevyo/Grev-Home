27825338a60c4b3c840720c81efaa1b423bedf33
Grevyo <127150029+Grevyo@users.noreply.github.com>
2026-09-05 17:41:32 +0100

    Import controller input for presentation feedback

--- src/GrevHome/MainWindow.ShellPresentation.cs
+++ src/GrevHome/MainWindow.ShellPresentation.cs
@@ -5,6 +5,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Threading;
+using GrevHome.Input;
 using GrevHome.Navigation;
 using GrevHome.Presentation;
 
